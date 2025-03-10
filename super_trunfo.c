73a77962c2d9553947c54641d169cee05c8d8c24
gusriber1 <gustavoribeiro10kk@gmail.com>
2025-03-10 18:36:14 -0300

    Update and rename CartasSuperTrunfo.c to super_trunfo.c

---
+++ super_trunfo.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+// Desafio Super Trunfo - Países
+// Tema 1 - Cadastro das cartas
+// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+
+int main(){
+
+// Área para definição das variáveis para armazenar as propriedades das cidades
+
+    char estado1, estado2;
+    char codigocarta1[5], codigocarta2[5];
+    char cidade1[20], cidade2[20];
+    int populacao1, populacao2;
+    float area1, area2;
+    float pib1, pib2;
+    int pontosturisticos1, pontosturisticos2;
+// Criação da Carta 1.
+printf("Vamos criar a Carta 1!\n\nDigite a letra do estado. (De 'A' a 'H'): ");
+scanf(" %c", &estado1);
+
+printf("Digite o código. (Letra do estado + dois números): ");
+scanf("%s", codigocarta1);
+
+printf("Digite o nome da cidade. (Sem espaços!): ");
+scanf("%s", cidade1);
+
+printf("Digite a população da cidade: ");
+scanf("%d", &populacao1);
+
+printf("Digite a população da cidade. (Em Km²): ");
+scanf("%f", &area1);
+
+printf("Digite o PIB da cidade: ");
+scanf("%f", &pib1);
+
+printf("Digite quantos pontos turísticos a cidade tem: ");
+scanf("%d", &pontosturisticos1);
+
+// Criação da carta 2.
+printf("\nCarta 1 criada com sucesso!\n\nAgora vamos criar a Carta 2!\n\n");
+
+printf("Digite a letra do estado. (De 'A' a 'H'): ");
+scanf(" %c", &estado2);
+
+printf("Digite o código. (Letra do estado + dois números): ");
+scanf("%s", codigocarta2);
+
+printf("Digite o nome da cidade. (Sem espaços!): ");
+scanf("%s", cidade2);
+
+printf("Digite a população da cidade: ");
+scanf("%d", &populacao2);
+
+printf("Digite a população da cidade. (Em Km²): ");
+scanf("%f", &area2);
+
+printf("Digite o PIB da cidade: ");
+scanf("%f", &pib2);
+
+printf("Digite quantos pontos turísticos a cidade tem: ");
+scanf("%d", &pontosturisticos2);
+
+printf("\nCarta 2 criada com sucesso!\n\n==============================\n\nAqui está os atributos das duas cartas:\n\n");
+
+// Área para exibição dos dados da cidade
+
+printf("Carta 1:\nEstado: %c\nCódigo: %s\nNome da cidade: %s\nPopulação: %d\nÁrea: %.3f\nPIB: %.3f\nNúmero de pontos turísticos: %d\n\n", estado1, codigocarta1, cidade1, populacao1, area1, pib1, pontosturisticos1);
+printf("==============================\n\n");
+printf("Carta 2:\nEstado: %c\nCódigo: %s\nNome da cidade: %s\nPopulação: %d\nÁrea: %.3f\nPIB: %.3f\nNúmero de pontos turísticos: %d\n\n", estado2, codigocarta2, cidade2, populacao2, area2, pib2, pontosturisticos2);
+
+return 0;
